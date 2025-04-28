ac986b0caf599cf5d6f4eee2da4bb56033b09b26
Wesley Teles Benette <wesleyteles.b@gmail.com>
2025-04-28 16:16:44 -0300

    [Wesley] init: Creates the Project.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "API Documentation - API de Filmes",
+        Version = "v1",
+        Description = "Uma API para servir um WebApp de Filmes.",
+        Contact = new OpenApiContact
+        {
+            Name = "Wesley T. Benette",
+            Email = "wesleyteles.b@gmail.com",
+            Url = new Uri("https://github.com/WesleyTelesBenette")
+        }
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.UseHttpsRedirection();
+
+app.Run();
